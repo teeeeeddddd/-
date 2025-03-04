6264708f3b723c911e9be727f1d7876af9efe8c0
twx2024 <twx20062024@outlook.com>
2025-03-04 23:20:09 +0800

    描述：例如 '添加用户手册文档'

---
+++ opencv_test.cpp
@@ -0,0 +1,20 @@
+﻿
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main(int argc, char ** argv) {
+	cv::Mat src = cv::imread("C:/Users/滕唯先/OneDrive/桌面/01.png", IMREAD_GRAYSCALE);
+	
+		if (src.empty()) {
+			printf("could not load image...");
+			return -1;
+		}
+	cv::imshow("my_window", src);
+		cv::waitKey(0);
+		destroyAllWindows();
+	system("pause");
+	return 0;
+}
